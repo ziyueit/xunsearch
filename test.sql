431484fb279329c3189bd8c983b5857bc59108f4
shusheng <imwiner@live.com>
2015-04-25 20:54:17 +0800

    测试的表结构

---
+++ test.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `test` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT '',
+  `text` text,
+  PRIMARY KEY (`cid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
