eafb3286533425026a6628ea9f552d3c62cf08cd
shusheng <imwiner@live.com>
2015-04-25 20:52:52 +0800

    清除索引操作

---
+++ clear.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: http://unun.in
+ * Date: 15-4-24
+ * Time: 下午10:23
+ */
+include './XunSearch.php';
+$xun = new XunSearch('test');
+$xun->clearIndex();
